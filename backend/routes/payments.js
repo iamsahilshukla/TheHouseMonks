a116a989135ff10f48f598410ab65ff027033393
Sahil Shukla <sahil.shukla@juspay.in>
2023-03-19 22:06:49 +0530

    added redirecting counter in views,Url as well

--- backend/routes/payments.js
+++ backend/routes/payments.js
@@ -15,9 +15,9 @@ router.post('/pay', async (req, res) => {
     // Function to generate access token
     async function generateAccessToken() {
         const formData = new FormData();
-        formData.append('grant_type', 'client_credentials');
-        formData.append('client_id', 'test_y0brvg5o0beAFd5wsREVjGfdnMF2Zkq4hza');  //need to put all this in config - confidential data
-        formData.append('client_secret', 'test_0p2I3qS574cRFjaJAfc6FSScnHzDQgAjrgBU6zmmMDTpBhL0EfMoNrFBzI1mc6TnsDm2BdcEy7zWNleyx9z8n2UhKOINlGlzbSXp3RQTMbsB4HSHzcrJbRxLHMV');  //need to put all this in config - confidential data
+        formData.append('grant_type', process.env.grant_type);
+        formData.append('client_id', process.env.client_id);
+        formData.append('client_secret', process.env.client_secret);
 
         const tokenResponse = await axios.post('https://test.instamojo.com/oauth2/token/', formData, {
             headers: {
@@ -60,31 +60,32 @@ router.post('/pay', async (req, res) => {
     // Integration steps
     (async function () {
         try {
-        // Generate access token
-        const authToken = await generateAccessToken();
-        console.log(`Access token: ${authToken}`);
+            // Generate access token
+            const authToken = await generateAccessToken();
+            console.log(`Access token: ${authToken}`);
 
-        // Create payment request
-        const purpose = invoice.description;
-        const amount = invoice.amount.toString();
-        const buyerName = invoice.name;
-        const email = invoice.email;
-        const phone = '9999999999';  //currently not taking data from user once we take data from user we can update in db and fetch it.
-        const redirectUrl = `https://thehousemonks.onrender.com/payments/${invoice_id}/success`;
-        const webhook = 'https://thehousemonks.onrender.com/webhooks/instamojo';
-        const allowRepeatedPayments = false;
+            // Create payment request
+            const purpose = invoice.description;
+            const amount = invoice.amount.toString();
+            const buyerName = invoice.name;
+            const email = invoice.email;
+            const phone = '9999999999';  //currently not taking data from user once we take data from user we can update in db and fetch it.
+            const redirectUrl = `https://thehousemonks.onrender.com/payments/${invoice_id}/success`;
+            const webhook = 'https://thehousemonks.onrender.com/webhooks/instamojo';
+            const allowRepeatedPayments = false;
 
-        const paymentRequestData = await createPaymentRequest(authToken, purpose, amount, buyerName, email, phone, redirectUrl, webhook, allowRepeatedPayments);
-        console.log(`Payment request created: ${JSON.stringify(paymentRequestData)}`);
+            const paymentRequestData = await createPaymentRequest(authToken, purpose, amount, buyerName, email, phone, redirectUrl, webhook, allowRepeatedPayments);
+            console.log(`Payment request created: ${JSON.stringify(paymentRequestData)}`);
 
-        res.json({
-            payment_request_id: paymentRequestData.id,
-            longurl: paymentRequestData.longurl
-        })}
-        catch(error) {
+            res.json({
+                payment_request_id: paymentRequestData.id,
+                longurl: paymentRequestData.longurl
+            })
+        }
+        catch (error) {
             console.log(error.message);
             res.status(500).send('Error sending payment request');
-    };
+        };
     })();
 
 });
--- frontend/script.js
+++ frontend/script.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
   // Get the table body elements
   const unpaidTableBody = document.querySelector('#unpaid-invoices');
   const paidTableBody = document.querySelector('#paid-invoices');
-  const payment_link = 'https://www.google.com'; // mock
+  const payment_link = 'https://www.google.com';  // dummy url
   // Fetch the invoice data from the server
-  fetch('http://localhost:3000/invoices/')
+  fetch('https://thehousemonks.onrender.com/invoices/')
     .then(response => response.json())
     .then(invoices => {
       // Loop through the invoices and add rows to the table
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
             var InvoiceId = button.dataset.invoiceid;
             console.log ("invoiceId");
             console.log (InvoiceId);
-            const response = await fetch('http://localhost:3000/payments/pay', {
+            const response = await fetch('https://thehousemonks.onrender.com/payments/pay', {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json'
